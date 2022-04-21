67ece7fcf263c3c8389ae355d65e459860858b73
Adrian Melian Fdez <alu0100974652@ull.edu.es>
2022-04-21 12:13:32 +0000

    Se puede acceder al titulo de la nota

--- src/note.ts
+++ src/note.ts
@@ -2,4 +2,8 @@ import * as chalk from 'chalk'
 
 export class Note {
     constructor(private title: string, private body: string, private color: string) {}
+
+    getTitle() {
+        return this.title
+    }
 }
